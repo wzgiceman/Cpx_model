41632a84087038a41088c6a5b473860920c033a3
shuoshuren <534361124@qq.com>
2018-11-15 09:03:48 +0800

    Dialog基础类添加公用方法

--- library/src/main/java/com/base/muslim/base/dialog/BaseDialog.java
+++ library/src/main/java/com/base/muslim/base/dialog/BaseDialog.java
@@ -1,15 +1,21 @@
 package com.base.muslim.base.dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.Toast;
 
 import com.base.library.R;
+import com.base.library.utils.AbStrUtil;
+import com.base.library.utils.AbToastUtil;
+import com.base.library.utils.FirebaseUtils;
 
 
 /**
@@ -18,6 +24,9 @@ import com.base.library.R;
  * @author WZG
  */
 public abstract class BaseDialog extends Dialog implements OnClickListener {
+
+
+    private ProgressDialog loadingDialog;
 
     public BaseDialog(Context context) {
         super(context);
@@ -67,21 +76,21 @@ public abstract class BaseDialog extends Dialog implements OnClickListener {
     }
 
     /**
-     * 显示基本信息
+     * 显示toast提示信息
      *
      * @param msg
      */
-    protected void showMsg(int msg) {
-        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+    protected void showToast(int msg) {
+        AbToastUtil.showToast(getContext(),msg);
     }
 
     /**
-     * 显示基本信息
+     * 显示toast提示信息
      *
      * @param msg
      */
-    protected void showMsg(String msg) {
-        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+    protected void showToast(String msg) {
+        AbToastUtil.showToast(getContext(),msg);
     }
 
     @Override
@@ -108,5 +117,102 @@ public abstract class BaseDialog extends Dialog implements OnClickListener {
         Intent intent = new Intent(getContext(), cls);
         intent.putExtras(bundle);
         getContext().startActivity(intent);
+    }
+
+
+
+    /**
+     * 显示统一的加载框
+     *
+     * @param cancel 是否可以取消
+     * @param title 显示的标题
+     */
+    protected void showLoading(boolean cancel, String title) {
+        if (!isValidActivity()) {
+            return;
+        }
+        String message = AbStrUtil.isEmpty(title) ? getString(R.string.Loading) : title;
+        if (loadingDialog == null) {
+            loadingDialog = ProgressDialog.show(getContext(), null, message);
+            loadingDialog.setCancelable(cancel);
+        } else if (loadingDialog != null && !loadingDialog.isShowing()) {
+            loadingDialog.setMessage(message);
+            loadingDialog.setCancelable(cancel);
+            loadingDialog.show();
+        }
+    }
+
+    /**
+     * 获取String.xml 中字符串
+     * @param id
+     * @return
+     */
+    public String getString(@StringRes int id){
+        return getContext().getResources().getString(id);
+    }
+
+    /**
+     * 获取String.xml 中字符串
+     * @param id
+     * @param formatArgs
+     * @return
+     */
+    public String getString(@StringRes int id, Object... formatArgs){
+        return getContext().getResources().getString(id,formatArgs);
+    }
+
+
+    /**
+     * 关闭加载框
+     */
+    protected void closeLoading() {
+        if (!isValidActivity()) {
+            return;
+        }
+        if (loadingDialog != null && loadingDialog.isShowing()) {
+            loadingDialog.dismiss();
+        }
+    }
+
+    /**
+     * 判断Activity是否是合法
+     * @return
+     */
+    private boolean isValidActivity(){
+        Activity activity = getOwnerActivity();
+        if(null == activity){
+            return false;
+        }
+        if (activity.isDestroyed() || activity.isFinishing()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 统计埋点
+     *
+     * @param key
+     */
+    public void collectionFireabse(@NonNull int key) {
+        collectionFireabse(getString(key));
+    }
+
+    public void collectionFireabse(@NonNull String key) {
+        FirebaseUtils.getInstance().report(key);
+    }
+
+    /**
+     * 统计埋点
+     *
+     * @param key
+     * @param bundle
+     */
+    public void collectionFireabse(@NonNull int key, Bundle bundle) {
+        collectionFireabse(getString(key), bundle);
+    }
+
+    public void collectionFireabse(@NonNull String key, Bundle bundle) {
+        FirebaseUtils.getInstance().report(key, bundle);
     }
 }
