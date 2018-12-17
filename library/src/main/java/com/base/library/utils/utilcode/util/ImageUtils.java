e0884ae4333da9c34e38203be596b634ae4d0997
shuoshuren <534361124@qq.com>
2018-12-17 10:08:55 +0800

    添加对view进行截图的方法

--- library/src/main/java/com/base/library/utils/utilcode/util/ImageUtils.java
+++ library/src/main/java/com/base/library/utils/utilcode/util/ImageUtils.java
@@ -158,6 +158,42 @@ public final class ImageUtils {
     }
 
     /**
+     * 对view 进行截图
+     *
+     * @param view The view.
+     * @return bitmap
+     */
+    public static Bitmap getMagicDrawingCache(final View view) {
+        Bitmap bitmap = (Bitmap) view.getTag(R.id.CACHE_BITMAP_KEY);
+        Boolean dirty = (Boolean) view.getTag(R.id.CACHE_BITMAP_DIRTY_KEY);
+        if (view.getWidth() == 0 && view.getHeight() == 0) {
+            view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec
+                    .UNSPECIFIED));
+            view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        }
+        int width = view.getWidth();
+        int height = view.getHeight();
+        if (bitmap == null || bitmap.getWidth() != width || bitmap.getHeight() != height) {
+            if (bitmap != null && !bitmap.isRecycled()) {
+                bitmap.recycle();
+            }
+            try{
+                bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            view.setTag(R.id.CACHE_BITMAP_KEY, bitmap);
+            dirty = true;
+        }
+        if (dirty == true) {
+            Canvas canvas = new Canvas(bitmap);
+            view.draw(canvas);
+            view.setTag(R.id.CACHE_BITMAP_DIRTY_KEY, false);
+        }
+        return bitmap;
+    }
+
+    /**
      * View to bitmap.
      *
      * @param view The view.
