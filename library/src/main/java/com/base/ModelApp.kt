5daab4257bf42f1bd48ba05f84d515f7f12c6dce
zhigangwei <zgwei@zhigangweideMacBook-Pro.local>
2018-12-21 15:25:31 +0800

    update isValid http

--- library/src/main/java/com/base/ModelApp.kt
+++ library/src/main/java/com/base/ModelApp.kt
@@ -34,7 +34,7 @@ object ModelApp {
                 .setLog2FileSwitch(false)// 打印log时是否存到文件的开关，默认关
                 .setDir("")// 当自定义路径为空时，写入应用的/cache/logEvent/目录中
                 .setFilePrefix("")// 当文件前缀为空时，默认为"util"，即写入文件为"util-MM-dd.txt"
-                .setBorderSwitch(false)// 输出日志是否带边框开关，默认开
+                .setBorderSwitch(true)// 输出日志是否带边框开关，默认开
                 .setConsoleFilter(LogUtils.D)// log的控制台过滤器，和logcat过滤器同理，默认Verbose
                 .setFileFilter(LogUtils.D)// log文件过滤器，和logcat过滤器同理，默认Verbose
                 .setStackDeep(1)// log栈深度，默认为1
