36a80c6ae97758c51f8311bd2120b47fa816a6f0
wangkaixuan <wangkaixuanjc@gmail.com>
2019-01-25 00:18:03 +0800

    optimize BaseLazyFragment

--- library/src/main/java/com/base/project/base/fragment/BaseLazyFragment.kt
+++ library/src/main/java/com/base/project/base/fragment/BaseLazyFragment.kt
@@ -1,6 +1,5 @@
 package com.base.project.base.fragment
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +9,8 @@ import com.base.project.base.IBase
 /**
  * Description:
  * Fragment基类，懒加载
+ * 当数据加载完成后调用[loadingFinish],Fragment重建时便不会再次调用[initData]，只会调用[initView]将数据设置上去
+ * 当数据加载取消/失败后调用[resetLoadingStatus]，使得Fragment重建时再次调用[initData]加载数据
  *
  * @author  Alpinist Wang
  * Company: Mobile CPX
@@ -35,24 +36,24 @@ abstract class BaseLazyFragment : BaseSaveFragment(), IBase {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loadData()
+        lazyLoad()
     }
 
 
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
-        loadData()
+        lazyLoad()
     }
 
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
         super.setUserVisibleHint(isVisibleToUser)
-        loadData()
+        lazyLoad()
     }
 
     /**
-     * 加载数据
+     * 当页面可见且尚未加载过数据时，加载数据
      */
-    private fun loadData() {
+    private fun lazyLoad() {
         if (checkVisible() && loadingStatus == LoadingStatusType.NotLoadYet) {
             loadingStatus = LoadingStatusType.Loading
             initData()
@@ -61,11 +62,12 @@ abstract class BaseLazyFragment : BaseSaveFragment(), IBase {
     }
 
     /**
-     * 确定Fragment是否可见
+     * 检查Fragment是否可见
      * 比系统的[isVisible]方法少了 view.windowToken == null 判断
      * 由于commit提交的Fragment的view.windowToken初始时为null，导致[isVisible]为false，无法懒加载数据
-     * TODO 当然，最好的方法是使当前显示的Fragment的view.windowToken变为非空，暂时没有找到解决方法
      *
+     * 注：当然，最好的方案是使commit提交的Fragment的view.windowToken变为非空，不过暂时没有找到解决方法，
+     * 当前方案在ViewPager和commit时暂未发现问题
      */
     private fun checkVisible(): Boolean {
         return isAdded && !isHidden && view != null && view?.visibility == View.VISIBLE
@@ -74,19 +76,18 @@ abstract class BaseLazyFragment : BaseSaveFragment(), IBase {
     /**
      * 恢复数据
      * fragment销毁时，fragment中的数据不会被销毁
-     * 当fragment重新创建时，将数据再次设置到view中
+     * 当fragment重建时，将数据再次设置到view中
      */
     override fun onRestoreState(savedInstanceState: Bundle) {
         super.onRestoreState(savedInstanceState)
         initView()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // 如果数据仍然在加载，View就被销毁了，则重置loadingStatus，使得下次到达此页面重新加载数据
-        if (loadingStatus == LoadingStatusType.Loading) {
-            resetLoadingStatus()
-        }
+    /**
+     * 加载状态设置为加载完成
+     */
+    open fun loadingFinish() {
+        loadingStatus = LoadingStatusType.LoadFinish
     }
 
     /**
@@ -97,10 +98,11 @@ abstract class BaseLazyFragment : BaseSaveFragment(), IBase {
         loadingStatus = LoadingStatusType.NotLoadYet
     }
 
-    /**
-     * 加载状态设置为加载完成
-     */
-    open fun loadingFinish() {
-        loadingStatus = LoadingStatusType.LoadFinish
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // 如果数据仍然在加载，View就被销毁了，则重置loadingStatus，使得Fragment重建时重新加载数据
+        if (loadingStatus == LoadingStatusType.Loading) {
+            resetLoadingStatus()
+        }
     }
 }
